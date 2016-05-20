d0730f68eabba008605e721af3402ad5601cb64a
dribehance <dom4j.for.networking@gmail.com>
2016-05-20 18:32:16 +0800

    skillopediaServices ,coursesServices

---
+++ src/js/services/coursesServices.js
@@ -0,0 +1,25 @@
+// by dribehance <dribehance.kksdapp.com>
+angular.module("Skillopedia").factory("coursesServices", function($http, config) {
+	return {
+		query: function(input) {
+			return $http({
+				// by dribehance <dribehance.kksdapp.com>
+				url: config.url + "interface",
+				method: "GET",
+				params: angular.extend({}, config.common_params, input)
+			}).then(function(data) {
+				return data.data;
+			});
+		},
+		query_by_id: function(input) {
+			return $http({
+				// by dribehance <dribehance.kksdapp.com>
+				url: config.url + "interface",
+				method: "GET",
+				params: angular.extend({}, config.common_params, input)
+			}).then(function(data) {
+				return data.data;
+			});
+		}
+	}
+});
---
+++ src/js/services/skillopediaServices.js
@@ -0,0 +1,45 @@
+// by dribehance <dribehance.kksdapp.com>
+angular.module("Skillopedia").factory("skillopediaServices", function($http, config) {
+	return {
+		query_banner: function(input) {
+			return $http({
+				// by dribehance <dribehance.kksdapp.com>
+				url: config.url + "interface",
+				method: "GET",
+				params: angular.extend({}, config.common_params, input)
+			}).then(function(data) {
+				return data.data;
+			});
+		},
+		query_base_category: function(input) {
+			return $http({
+				// by dribehance <dribehance.kksdapp.com>
+				url: config.url + "interface",
+				method: "GET",
+				params: angular.extend({}, config.common_params, input)
+			}).then(function(data) {
+				return data.data;
+			});
+		},
+		query_recommand_category: function(input) {
+			return $http({
+				// by dribehance <dribehance.kksdapp.com>
+				url: config.url + "interface",
+				method: "GET",
+				params: angular.extend({}, config.common_params, input)
+			}).then(function(data) {
+				return data.data;
+			});
+		},
+		query_hot_category: function(input) {
+			return $http({
+				// by dribehance <dribehance.kksdapp.com>
+				url: config.url + "interface",
+				method: "GET",
+				params: angular.extend({}, config.common_params, input)
+			}).then(function(data) {
+				return data.data;
+			});
+		}
+	}
+});
