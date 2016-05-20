dc37af5365973d22a8ad066401392b7620906e4b
dribehance <dom4j.for.networking@gmail.com>
2016-05-20 12:12:00 +0800

    add toast support and progress support

--- src/js/controllers/indexController.js
+++ src/js/controllers/indexController.js
@@ -1,4 +1,4 @@
 // by dribehance <dribehance.kksdapp.com>
 angular.module("Skillopedia").controller("indexController", function($scope, errorServices, toastServices, localStorageService, config) {
-
+	toastServices.start();
 });
