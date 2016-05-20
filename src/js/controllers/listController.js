71ba5cd0f41ae6009e20acf89a1a3e503818b4d4
dribehance <dom4j.for.networking@gmail.com>
2016-05-20 10:35:27 +0800

    condition interactive

--- src/js/controllers/listController.js
+++ src/js/controllers/listController.js
@@ -1,4 +1,26 @@
 // by dribehance <dribehance.kksdapp.com>
 angular.module("Skillopedia").controller("listController", function($scope, errorServices, toastServices, localStorageService, config) {
-
+	$scope.input = {
+		categorys: [
+			"football",
+			"basketball",
+			"swimming"
+		],
+		distances: [
+			"500-1000mile",
+			"1000-1500mile",
+			"1500-2000mile",
+			"2000-2500mile",
+			"2500-3000mile"
+		],
+		priorities: [
+			"distance",
+			"price",
+			"review",
+			"hot",
+		]
+	}
+	$scope.remove = function(condition) {
+		$scope.input[condition] = "";
+	}
 })
