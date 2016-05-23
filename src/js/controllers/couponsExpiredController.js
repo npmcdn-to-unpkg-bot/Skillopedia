164e12462db040d1c48fa3db4dcb5b3124b89a86
dribehance <dom4j.for.networking@gmail.com>
2016-05-23 16:35:49 +0800

    fix coupons_expired

--- src/js/controllers/couponsExpiredController.js
+++ src/js/controllers/couponsExpiredController.js
@@ -1,4 +1,4 @@
 // by dribehance <dribehance.kksdapp.com>
-angular.module("Skillopedia").controller("couponExpiredController", function($scope, errorServices, toastServices, localStorageService, config) {
+angular.module("Skillopedia").controller("couponsExpiredController", function($scope, errorServices, toastServices, localStorageService, config) {
 
 })
