bdc37dd601548996b277c53340f0fd7ced4c4cbe
dribehance <dom4j.for.networking@gmail.com>
2016-05-23 11:46:24 +0800

    fix create course

--- src/js/controllers/createCourseController.js
+++ src/js/controllers/createCourseController.js
@@ -1,4 +1,15 @@
 // by dribehance <dribehance.kksdapp.com>
 angular.module("Skillopedia").controller("createCourseController", function($scope, errorServices, toastServices, localStorageService, config) {
-
+	console.log("dd")
+	$scope.step = 1;
+	$scope.show_step = function(step) {
+		$scope.step = step;
+	}
+	$scope.location_mode = "edit";
+	$scope.save = function() {
+		$scope.location_mode = "preview";
+	}
+	$scope.edit = function() {
+		$scope.location_mode = "edit";
+	}
 })
