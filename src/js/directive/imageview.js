e8f3658e199213972e2311403f1dc8338e2a46e0
dribehance <dom4j.for.networking@gmail.com>
2016-07-19 15:56:11 +0800

    -

--- src/js/directive/imageview.js
+++ src/js/directive/imageview.js
@@ -1,32 +1,131 @@
-angular.module("Skillopedia").directive('imageview', function() {
-	return {
-		restrict: 'E',
-		scope: {
-			src: "="
-		},
-		template: "<img ng-src='{{src}}' show-on-loaded>",
-		link: function(scope, element, attrs) {
-			var bg_image = "../images/default.png";
-			var rate = parseFloat(scope.$eval($(element).attr('data-rate')));
-			if (!rate) {
-				console.log("unexpect rate")
-				return;
+angular.module("Skillopedia")
+	.directive('imageview', function($timeout) {
+		return {
+			restrict: 'E',
+			scope: {
+				src: "="
+			},
+			controller: function($scope, $element, $attrs) {
+				$scope.bg_image = "../images/default.png";
+				$scope.rate = parseFloat($attrs.rate);
+				$scope.width = $element.parent().width() || $(window).width();
+				$scope.height = $scope.width / $scope.rate;
+				if (!$scope.rate) {
+					console.log("unexpect rate")
+					return;
+				}
+				this.show_center_on_loaded = function(e) {
+					calculate_center_image_size_on_loaded(e);
+					$timeout(function() {
+						calculate_center_image_size_on_loaded(e);
+					}, 100)
+				}
+				this.show_on_loaded = function(e) {
+					calculate_image_size_on_loaded(e);
+					$timeout(function() {
+						calculate_image_size_on_loaded(e);
+					}, 100)
+				}
+
+				function calculate_center_image_size_on_loaded(e) {
+					$(e.target).css({
+						width: 'auto',
+						height: 'auto'
+					})
+					var image_loaded = $(e.target),
+						image_loaded_width = image_loaded.width(),
+						image_loaded_height = image_loaded.height(),
+						actural_rate = parseFloat(image_loaded_width) / parseFloat(image_loaded_height);
+					if (actural_rate < $scope.rate) {
+						image_loaded_width = $scope.width;
+						image_loaded_height = $scope.width / actural_rate
+						image_loaded.css({
+							"display": "inline-block",
+							"height": image_loaded_height,
+							"width": image_loaded_width,
+							"margin-top": -(image_loaded_height - $scope.height) / 2
+						})
+					} else {
+						image_loaded_height = $scope.height
+						image_loaded_width = image_loaded_height * actural_rate;
+						image_loaded.css({
+							"display": "inline-block",
+							"height": image_loaded_height,
+							"width": image_loaded_width,
+							"margin-left": -(image_loaded_width - $scope.width) / 2
+						})
+					}
+					image_loaded.parent().css({
+						"background": "none"
+					})
+					image_loaded.css({
+						"visibility": "visible"
+					})
+				}
+
+				function calculate_image_size_on_loaded(e) {
+					var image_loaded = $(e.target),
+						image_loaded_width = image_loaded.width(),
+						image_loaded_height = image_loaded.height(),
+						actural_rate = parseFloat(image_loaded_width) / parseFloat(image_loaded_height);
+					if (actural_rate < $scope.rate) {
+						image_loaded.css({
+							"display": "inline-block",
+							"height": "100%",
+							"width": "auto"
+						})
+					} else {
+						image_loaded.css({
+							"display": "inline-block",
+							"height": "auto",
+							"width": "100%"
+						})
+					}
+					image_loaded.parent().css({
+						"background": "none"
+					})
+					image_loaded.css({
+						"visibility": "visible"
+					})
+				}
+			},
+			template: function(element, attrs) {
+				var template = "<img ng-src='{{src}}' show-center-on-loaded err-src='../images/banner.png'>";
+				if ($(element).attr('center-only') == undefined) {
+					template = "<img ng-src='{{src}}' show-on-loaded err-src='../images/banner.png'>"
+				}
+				return template;
+			},
+			link: function(scope, element, attrs, ctrl) {
+				var style = {
+					display: "block",
+					width: scope.width,
+					overflow: "hidden",
+					"text-align": "center",
+					"background-image": "url(" + scope.bg_image + ")",
+					"background-position": "center center",
+					"background-repeat": "no-repeat",
+					"height": scope.height,
+					"overflow": "hidden"
+				}
+				$(element).css(style);
 			}
-			var style = {
-				display: "block",
-				width: $(element).width() || $(element).parent().width(),
-				overflow: "hidden",
-				"text-align": "center",
-				"background-image": "url(" + bg_image + ")",
-				"background-size": "100%",
-				"background-position": "center center",
-				"line-height": ($(element).width() || $(element).parent().width()) / rate + "px",
-				"height": ($(element).width() || $(element).parent().width()) / rate
+		};
+	})
+	.directive('showOnLoaded', function() {
+		return {
+			restrict: "A",
+			require: "^imageview",
+			link: function(scope, element, attrs, ctrl) {
+				element.bind('load', ctrl.show_on_loaded);
 			}
-			$(element).css(style);
-			$(element).parent().css({
-				overflow: "hidden"
-			});
 		}
-	};
-});
+	}).directive('showCenterOnLoaded', function() {
+		return {
+			restrict: "A",
+			require: "^imageview",
+			link: function(scope, element, attrs, ctrl) {
+				element.bind('load', ctrl.show_center_on_loaded);
+			}
+		}
+	});
