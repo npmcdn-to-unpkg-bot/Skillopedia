4660d42641d861b461f0b695b959042db3d0b7cf
dribehance <dom4j.for.networking@gmail.com>
2016-07-27 15:46:45 +0800

    fix dropdown menu

--- src/js/controllers/headerController.js
+++ src/js/controllers/headerController.js
@@ -1,12 +1,17 @@
 // by dribehance <dribehance.kksdapp.com>
 angular.module("Skillopedia").controller("headerController", function($scope, $route, userServices, errorServices, toastServices, localStorageService, config) {
-	$scope.show_menu = function() {
+	$scope.show_menu = function(e) {
 		if ($(".dropdown-menu").hasClass("active")) {
 			$(".dropdown-menu").removeClass("active")
 		} else {
 			$(".dropdown-menu").addClass("active")
 		}
+		e.preventDefault();
+		e.stopPropagation();
 	}
+	$("body").bind("click", function() {
+		$(".dropdown-menu").removeClass("active");
+	})
 	$scope.logout = function() {
 		userServices.logout();
 		$route.reload();
