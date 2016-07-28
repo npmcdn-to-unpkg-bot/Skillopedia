49c3b8d4be7ba7761d68982b712c53bf50aeee91
dribehance <dom4j.for.networking@gmail.com>
2016-07-28 11:43:23 +0800

    fix image too big

--- src/js/controllers/accountController.js
+++ src/js/controllers/accountController.js
@@ -30,6 +30,7 @@ angular.module("Skillopedia").controller("accountController", function($scope, $
 });
 angular.module("Skillopedia").controller("uploadAvatarController", function($scope, $rootScope, userServices, errorServices, toastServices, localStorageService, config) {
 	$scope.$on("flow::filesSubmitted", function(event, flow, flowFile) {
+		if (flow.files.length == 0) return;
 		// $rootScope.back();
 		flow.opts.target = config.url + "/app/UserCenter/UpdateAvatar";
 		flow.opts.testChunks = false;
@@ -39,4 +40,22 @@ angular.module("Skillopedia").controller("uploadAvatarController", function($sco
 		};
 		flow.upload();
 	})
+	$scope.$on('flow::fileAdded', function(event, flowFile, flow) {
+		if (!{
+				png: 1,
+				gif: 1,
+				jpg: 1,
+				jpeg: 1
+			}[flow.getExtension()]) {
+			errorServices.autoHide("必须上传图片")
+			event.preventDefault(); //prevent file from uploading
+			return;
+		}
+		if (parseFloat(flow.size) / 1000 > 500) {
+			errorServices.autoHide("图片太大，保证图片在500kb以内")
+			event.preventDefault(); //prevent file from uploading
+			return;
+		}
+		$scope.cert.url = "";
+	});
 })
